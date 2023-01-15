RestComm/jain-sip

Update https://github.com/RestComm/jain-sip/issues/116
More fixes to clean up logentries

src/gov/nist/javax/sip/stack/SIPTransactionStack.java
@@ -2563,12 +2563,12 @@ public abstract class SIPTransactionStack implements
                     newChannel = nextProcessor
                             .createMessageChannel(targetHostPort);
                 } catch (UnknownHostException ex) {
-                    if (logger.isLoggingEnabled())
-                        logger.logException(ex);
+                    if (logger.isLoggingEnabled(LogWriter.TRACE_DEBUG))
+                        logger.logDebug("host is not known " + targetHostPort + " " + ex.getMessage());
                     throw ex;
                 } catch (IOException e) {
-                    if (logger.isLoggingEnabled())
-                        logger.logException(e);
+                    if (logger.isLoggingEnabled(LogWriter.TRACE_DEBUG))
+                    	logger.logDebug("host is reachable " + targetHostPort + " " + ex.getMessage());
                     // Ignore channel creation error -
                     // try next processor
                 }

kpouer/jsip

(cherry picked from commit fc67718da6995bea4e6d4bf052396e61843cd1a3)

src/gov/nist/javax/sip/stack/SIPTransactionStack.java
@@ -2538,12 +2538,12 @@ public abstract class SIPTransactionStack implements
                     newChannel = nextProcessor
                             .createMessageChannel(targetHostPort);
                 } catch (UnknownHostException ex) {
-                    if (logger.isLoggingEnabled())
-                        logger.logException(ex);
+                    if (logger.isLoggingEnabled(LogWriter.TRACE_DEBUG))
+                        logger.logDebug("host is not known " + targetHostPort + " " + ex.getMessage());
                     throw ex;
                 } catch (IOException e) {
-                    if (logger.isLoggingEnabled())
-                        logger.logException(e);
+                    if (logger.isLoggingEnabled(LogWriter.TRACE_DEBUG))
+                        logger.logDebug("host is reachable " + targetHostPort + " " + e.getMessage());
                     // Ignore channel creation error -
                     // try next processor
                 }